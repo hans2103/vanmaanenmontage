05024eb99fea3718282818e208c4196f1d36044b
Hans Kuijpers <hans2103@gmail.com>
2025-03-17 17:16:13 +0100

    fix container issue

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -6,44 +6,45 @@ type ContainerProps = {
     children: ReactNode;
 } & React.HTMLAttributes<HTMLDivElement>;
 
+// OuterContainer component
 const OuterContainer = forwardRef<HTMLDivElement, ContainerProps>(
-    ({ className, children, ...props }, ref) => {
-        return (
-            <div ref={ref} className={cn('sm:px-8', className)} {...props}>
-                <div className="mx-auto max-w-7xl lg:px-8">{children}</div>
-            </div>
-        );
-    }
+    ({ className, children, ...props }, ref) => (
+        <div ref={ref} className={cn('sm:px-8', className)} {...props}>
+            <div className="mx-auto max-w-7xl lg:px-8">{children}</div>
+        </div>
+    )
 );
-
 OuterContainer.displayName = 'OuterContainer';
 
+// InnerContainer component
 const InnerContainer = forwardRef<HTMLDivElement, ContainerProps>(
-    ({ className, children, ...props }, ref) => {
-        return (
-            <div
-                ref={ref}
-                className={cn('relative px-4 sm:px-8 lg:px-12', className)}
-                {...props}
-            >
-                <div className="mx-auto max-w-2xl lg:max-w-5xl">{children}</div>
-            </div>
-        );
-    }
+    ({ className, children, ...props }, ref) => (
+        <div ref={ref} className={cn('relative px-4 sm:px-8 lg:px-12', className)} {...props}>
+            <div className="mx-auto max-w-2xl lg:max-w-5xl">{children}</div>
+        </div>
+    )
 );
-
 InnerContainer.displayName = 'InnerContainer';
 
-const Container = Object.assign(forwardRef<HTMLDivElement, ContainerProps>(
-    ({ children, ...props }, ref) => {
-        return (
-            <OuterContainer ref={ref} {...props}>
-                <InnerContainer>{children}</InnerContainer>
-            </OuterContainer>
-        );
-    }
-), { Outer: OuterContainer, Inner: InnerContainer });
+// Main Container component (combining OuterContainer and InnerContainer)
+const Container = forwardRef<HTMLDivElement, ContainerProps>(
+    ({ children, className, ...props }, ref) => (
+        <OuterContainer ref={ref} className={className} {...props}>
+            <InnerContainer>{children}</InnerContainer>
+        </OuterContainer>
+    )
+);
+Container.displayName = 'Container';
 
-Container.displayName = 'Container'; // Manually set displayName
+// Explicitly type the Container component with Outer and Inner properties
+interface ContainerWithSubcomponents extends React.ForwardRefExoticComponent<ContainerProps> {
+    Outer: typeof OuterContainer;
+    Inner: typeof InnerContainer;
+}
 
-export default Container;
+// Add subcomponents to the Container component
+const ContainerWithSubcomponents = Container as ContainerWithSubcomponents;
+ContainerWithSubcomponents.Outer = OuterContainer;
+ContainerWithSubcomponents.Inner = InnerContainer;
+
+export default ContainerWithSubcomponents;
