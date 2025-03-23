a1b6c2f2cb047de14456ed9836bb448148edeed6
Hans Kuijpers <hans2103@gmail.com>
2025-03-23 16:41:22 +0100

    add creator of website

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import {cn} from "@/lib/utils";
+import Link from "next/link";
 
 const Footer = () => {
     return (
         <footer className="py-4 bg-gray-800 text-white text-center">
-            <p>&copy; {new Date().getFullYear()} van Maanen Montage</p>
+            <p>&copy; {new Date().getFullYear()} van Maanen Montage - website gemaakt door <Link
+                href={'https://hkweb.nl'}
+                className={cn(
+                    'hover:text-sky-500 dark:hover:text-sky-400 underline underline-offset-4',
+                )}
+            >
+                HKweb.nl
+            </Link></p>
         </footer>
     )
 };
